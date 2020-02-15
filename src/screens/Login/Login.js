05e67fb12419b452b9d73ade9ff781c38f3cdd02
João Manoel Neto <joaaomanooel@gmail.com>
2020-02-15 11:53:51 -0300

    connect login with api

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Alert } from 'react-native';
 import { t } from '@/i18n';
 import { Input, Header, Button, TrincaLogo } from '@/components';
 import { Container, FormContainer } from './StyledComponent';
 
-export default React.memo(() => {
+export default React.memo(({ navigation, login, loading, error, user }) => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleLogin = () => login({ email, password });
+
+  useEffect(() => {
+    error && Alert.alert(t('error'), t('loginError'));
+  }, [error]);
+
+  useEffect(() => {
+    user._id && navigation.navigate('Main');
+  }, [user]);
   return (
     <Container>
       <Header />
       <FormContainer>
-        <Input label={t('email')} placeholder={t('email')} autoCompleteType="email" />
         <Input
-          secureTextEntry
-          placeholder={t('password')}
-          label={t('password')}
-          autoCompleteType="password"
+          value={email}
+          label={t('email')}
+          onChangeText={setEmail}
+          placeholder={t('email')}
+          autoCompleteType="email"
         />
-        <Button text={t('enter').toUpperCase()} />
+        <Input
+          placeholder={t('password')}
+          autoCompleteType="password"
+          onChangeText={setPassword}
+          label={t('password')}
+          value={password}
+          secureTextEntry
+        />
+        <Button disabled={loading} text={t('enter').toUpperCase()} onPress={handleLogin} />
       </FormContainer>
       <TrincaLogo />
     </Container>
